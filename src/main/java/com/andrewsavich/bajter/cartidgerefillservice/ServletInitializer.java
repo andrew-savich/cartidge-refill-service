1e532d8f18a6dabf7e04c3042ec0e6d6defbb935
andrew-savich <savichandrew@Gmail.com>
2021-09-10 17:39:25 +0300

    name correction

--- src/main/java/com/andrewsavich/bajter/cartidgerefillservice/ServletInitializer.java
+++ src/main/java/com/andrewsavich/bajter/cartidgerefillservice/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(CartidgeRefillServiceApplication.class);
+		return application.sources(CartridgeRefillServiceApplication.class);
 	}
 
 }
