e02fea572b208f0b8826ff7ca691cb80a8e4cd5a
andrew-savich <savichandrew@Gmail.com>
2021-09-29 11:17:02 +0300

    Validation pojo employee

--- src/main/java/com/andrewsavich/bajter/cartridgerefillservice/model/employee/Employee.java
+++ src/main/java/com/andrewsavich/bajter/cartridgerefillservice/model/employee/Employee.java
@@ -6,6 +6,10 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 @Entity
 @Data
@@ -17,11 +21,31 @@ public class Employee {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Min(5)
+    @Max(30)
+    @Pattern(regexp = "^[a-z0-9_-]*$")
     private String login;
+
+    @NotNull
+    @Min(5)
+    @Max(30)
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
     private String password;
+
+    @NotNull
+    @Min(2)
+    @Max(30)
+    @Pattern(regexp = "^[а-яА-Яa-zA-Z]*$")
     private String firstName;
+
+    @NotNull
+    @Min(2)
+    @Max(30)
+    @Pattern(regexp = "^[а-яА-Яa-zA-Z]*$")
     private String lastName;
 
+    @NotNull
     @Enumerated(value = EnumType.STRING)
     private Position position;
 
