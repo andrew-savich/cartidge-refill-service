9dfafe50bd9ca8d057ab05ebb40cf155ba013f1f
andrew-savich <savichandrew@Gmail.com>
2021-09-29 14:57:34 +0300

    Added client repository

---
+++ src/main/java/com/andrewsavich/bajter/cartridgerefillservice/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package com.andrewsavich.bajter.cartridgerefillservice.repository;
+
+import com.andrewsavich.bajter.cartridgerefillservice.model.client.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
