54fd2f11cf1270f29b81ac8f817740f578ce6190
andrew-savich <savichandrew@Gmail.com>
2021-09-29 15:26:35 +0300

    Refactor employee repository

--- src/main/java/com/andrewsavich/bajter/cartridgerefillservice/repository/EmployeeRepository.java
+++ src/main/java/com/andrewsavich/bajter/cartridgerefillservice/repository/EmployeeRepository.java
@@ -1,4 +1,4 @@
-package com.andrewsavich.bajter.cartridgerefillservice.dao;
+package com.andrewsavich.bajter.cartridgerefillservice.repository;
 
 import com.andrewsavich.bajter.cartridgerefillservice.model.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
