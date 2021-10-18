eedbf8af77273c45e335204e4bb7ed5adf0adb31
andrew-savich <savichandrew@Gmail.com>
2021-10-19 00:36:57 +0300

    Created ModelService

---
+++ src/main/java/com/andrewsavich/bajter/cartridgerefillservice/service/model/ModelService.java
@@ -0,0 +1,13 @@
+package com.andrewsavich.bajter.cartridgerefillservice.service.model;
+
+import com.andrewsavich.bajter.cartridgerefillservice.model.cartridge.Model;
+
+import java.util.List;
+
+public interface ModelService {
+    List<Model> getAllModels();
+    Model getModelById(Long id);
+    Model saveModel(Model model);
+    void deleteModel(Model model);
+    boolean isExistModelTitle(Model model);
+}
