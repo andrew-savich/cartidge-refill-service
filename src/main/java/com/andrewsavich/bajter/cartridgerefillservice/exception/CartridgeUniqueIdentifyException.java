f5ef09d4f3684c1285be09ae28b4392fc25c49f4
andrew-savich <savichandrew@Gmail.com>
2021-10-23 23:18:20 +0300

    Cartridge unique identify exception

---
+++ src/main/java/com/andrewsavich/bajter/cartridgerefillservice/exception/CartridgeUniqueIdentifyException.java
@@ -0,0 +1,7 @@
+package com.andrewsavich.bajter.cartridgerefillservice.exception;
+
+public class CartridgeUniqueIdentifyException extends RuntimeException{
+    public CartridgeUniqueIdentifyException(String message) {
+        super(message);
+    }
+}
