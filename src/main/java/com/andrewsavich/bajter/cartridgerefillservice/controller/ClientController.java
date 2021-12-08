e8c9c2dca556f332ef20505a4e07fdc0f89f1080
andrew-savich <savichandrew@Gmail.com>
2021-12-08 15:57:47 +0300

    Improved client controller rest api

--- src/main/java/com/andrewsavich/bajter/cartridgerefillservice/controller/ClientController.java
+++ src/main/java/com/andrewsavich/bajter/cartridgerefillservice/controller/ClientController.java
@@ -3,75 +3,92 @@ package com.andrewsavich.bajter.cartridgerefillservice.controller;
 import com.andrewsavich.bajter.cartridgerefillservice.exception.ClientNameExistsException;
 import com.andrewsavich.bajter.cartridgerefillservice.model.client.Client;
 import com.andrewsavich.bajter.cartridgerefillservice.service.client.ClientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
+import javax.validation.Valid;
 import java.util.List;
-import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
-@RequestMapping("/client")
+@RequestMapping(value = "/api/v1/clients", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 public class ClientController {
 
-    @Autowired
-    private ClientService clientService;
+    private final ClientService clientService;
 
-    @GetMapping("/all")
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @Operation(summary = "Returning client list")
+    @ApiResponse(responseCode = "200", description = "Clients were found", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @GetMapping
     public List<Client> getClientList(){
+        log.info("Controller: getting client list");
+
         return clientService.getAllClients();
     }
 
-    @GetMapping("/get/{id}")
-    public ResponseEntity<Client> getClientById(@PathVariable Long id){
-        Client client = clientService.getClientById(id);
+    @Operation(summary = "Returning client by id")
+    @ApiResponse(responseCode = "200", description = "Client was found", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Client> getClientById(@PathVariable Long clientId){
+        log.info("Controller: getting client with id: " + clientId);
+        Client client = clientService.getClientById(clientId);
 
+        log.info("Controller: sending client with id: " + clientId);
         return ResponseEntity.ok(client);
     }
 
-
-    @GetMapping("/getByName/{clientName}")
+    @Operation(summary = "Returning client by name")
+    @ApiResponse(responseCode = "200", description = "Client was found", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @GetMapping("/name/{clientName}")
     public ResponseEntity<Client> getClientByName(@PathVariable String clientName){
+        log.info("Controller: getting client with name: " + clientName);
         Client client = clientService.getClientByName(clientName);
 
+        log.info("Controller: sending client with name: " + clientName);
         return ResponseEntity.ok(client);
     }
 
-    @PostMapping("/create")
-    public void createClient(@RequestBody Client client){
+    @Operation(summary = "Creating a new client")
+    @ApiResponse(responseCode = "200", description = "Client was created", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @PostMapping
+    public void createClient(@RequestBody @Valid Client client){
+        log.info("Controller: Got client for creating: " + client);
 
         if(clientService.isExistClientName(client)){
             throw new ClientNameExistsException("Client with name" + client.getName() + " already exist");
         }
 
-        clientService.saveClient(client);
+        clientService.createClient(client);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Client> updateClient(@RequestBody Client changedClient, @PathVariable Long id){
-        Client client = clientService.getClientById(id);
-        clientService.updateFields(client, changedClient);
+    @Operation(summary = "Updating client")
+    @ApiResponse(responseCode = "200", description = "Employee was updated", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @PutMapping
+    public void updateClient(@RequestBody @Valid Client client){
+        log.info("Controller: Got client for updating: " + client);
 
         if(clientService.isExistClientName(client)){
             throw new ClientNameExistsException("Client with name " + client.getName() + " already exist");
         }
 
-        Client updatedClient = clientService.saveClient(client);
-        return ResponseEntity.ok(updatedClient);
+        clientService.updateClient(client);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Map<String, Boolean>> deleteClient(@PathVariable Long id){
-        Client client = clientService.getClientById(id);
-        clientService.deleteClient(client);
+    @Operation(summary = "Deleting client by id")
+    @ApiResponse(responseCode = "200", description = "Client was deleted", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @DeleteMapping("/{clientId}")
+    public void deleteClient(@PathVariable Long clientId){
+        log.info("Controller: Deleting client with id: " + clientId);
 
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("Deleted", true);
-
-        return ResponseEntity.ok(response);
+        clientService.deleteClientById(clientId);
     }
 }
