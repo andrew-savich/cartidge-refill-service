f684eb1bef8ce2c226782ec45894c43251270981
andrew-savich <savichandrew@Gmail.com>
2021-12-08 15:19:25 +0300

    Improved employee api controller

--- src/main/java/com/andrewsavich/bajter/cartridgerefillservice/controller/EmployeeController.java
+++ src/main/java/com/andrewsavich/bajter/cartridgerefillservice/controller/EmployeeController.java
@@ -4,8 +4,13 @@ import com.andrewsavich.bajter.cartridgerefillservice.exception.EmployeeLoginExi
 import com.andrewsavich.bajter.cartridgerefillservice.model.employee.Employee;
 import com.andrewsavich.bajter.cartridgerefillservice.model.employee.Position;
 import com.andrewsavich.bajter.cartridgerefillservice.service.employee.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,64 +21,74 @@ import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:3000/")
 @RestController
-@RequestMapping("/employee")
+@RequestMapping(value = "/api/v1/employees", produces = MediaType.APPLICATION_JSON_VALUE)
 @Slf4j
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/all")
+    @Operation(summary = "Returning employee list")
+    @ApiResponse(responseCode = "200", description = "Employees were found", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @GetMapping
     public List<Employee> getEmployeeList(){
+        log.info("Controller: getting employee list");
+
         return employeeService.getAllEmployee();
     }
 
+    @Operation(summary = "Returning employee position list")
+    @ApiResponse(responseCode = "200", description = "Positions were found", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
     @GetMapping("/positions")
     public List<Position> getEmployeePositions(){
+        log.info("Controller: getting position list");
+
         return employeeService.getAllEmployeePositions();
     }
 
-    @PostMapping("/create")
-    public void createEmployee(@Valid @RequestBody Employee employee) {
+    @Operation(summary = "Returning employee by id")
+    @ApiResponse(responseCode = "200", description = "Employee was found", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @GetMapping("/{employeeId}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long employeeId){
+        log.info("Controller: getting employee with id: " + employeeId);
+        Employee employee = employeeService.getEmployeeById(employeeId);
 
-        if (employeeService.isExistSameLogin(employee)){
-            throw new EmployeeLoginExistsException("Employee with this login allreay exist!");
-        }
-
-        employeeService.saveEmployee(employee);
-    }
-
-    @GetMapping("/get/{id}")
-    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
-        Employee employee = employeeService.getEmployeeById(id);
-
+        log.info("Controller: sending employee with id: " + employeeId);
         return ResponseEntity.ok(employee);
     }
 
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @Valid @RequestBody Employee changedEmployee){
-        Employee employee = employeeService.getEmployeeById(id);
-        employeeService.updateFields(employee, changedEmployee);
-
-        if(employeeService.isExistSameLogin(employee)){
-            throw new EmployeeLoginExistsException("Employee with this login allreay exist!");
+    @Operation(summary = "Creating a new employee")
+    @ApiResponse(responseCode = "200", description = "Employee was created", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @PostMapping
+    public void createEmployee(@Valid @RequestBody Employee employee) {
+        log.info("Controller: Got employee for creating: " + employee);
+        if (employeeService.isExistSameLogin(employee)){
+            throw new EmployeeLoginExistsException("Employee with this login '" + employee.getLogin() + "' already exists!");
         }
 
-        Employee updatedEmployee = employeeService.saveEmployee(employee);
-
-        return ResponseEntity.ok(updatedEmployee);
+        employeeService.createEmployee(employee);
     }
 
-    @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Map<String, Boolean>> deleteEmployee(@PathVariable Long id){
-        Employee employee = employeeService.getEmployeeById(id);
+    @Operation(summary = "Updating employee")
+    @ApiResponse(responseCode = "200", description = "Employee was updated", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @PutMapping
+    public void updateEmployee(@RequestBody Employee employee){
+        log.info("Controller: Got employee for updating: " + employee);
 
-        employeeService.deleteEmployee(employee);
+        if(employeeService.isExistSameLogin(employee)){
+            throw new EmployeeLoginExistsException("Employee with this login '" + employee.getLogin() + "' already exists!");
+        }
 
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("Deleted", true);
+        employeeService.updateEmployee(employee);
+    }
 
-        return ResponseEntity.ok(response);
+    @Operation(summary = "Deleting employee by id")
+    @ApiResponse(responseCode = "200", description = "Employee was deleted", content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
+    @DeleteMapping("/{employeeId}")
+    public void deleteEmployeeById(@PathVariable Long employeeId){
+        log.info("Controller: Deleting employee with id: " + employeeId);
+
+        employeeService.deleteEmployeeById(employeeId);
     }
 
 }
